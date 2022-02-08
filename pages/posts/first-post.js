228b861b5235bcdf3c5d7e6eade8b9405523b769
Siddesh <siddesh@ajency.in>
2022-02-08 11:14:53 +0530

    Blog pages

---
+++ pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Head from 'next/head'
+// import Script from 'next/script'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            {/* <Script 
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad = {() => { console.log(`script loaded`) }}
+            /> */}
+            <h1>First Post</h1>
+        </Layout>
+    )
+}

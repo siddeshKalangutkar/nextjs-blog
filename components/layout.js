d3a07396c3a8dd2acc1ae628331a8f0c18ddd281
Siddesh <siddesh@ajency.in>
2022-02-08 16:12:47 +0530

    Changed index page title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Siddesh K'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Blogs'
 
 export default function Layout({ children, home }) {
   return (
